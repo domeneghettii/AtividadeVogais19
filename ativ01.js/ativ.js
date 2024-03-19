01966aec70800982e8afdccf39f35f4033e0d0ea
Luana <luanaa.domeneghettii@gmail.com>
2024-03-19 09:40:37 -0300

    Array Vogais

---
+++ ativ01.js/ativ.js
@@ -0,0 +1,11 @@
+let frase = 'Olá, tenha um bom dia!';
+let minuscula =  frase.toLowerCase();
+let separaçao = minuscula.split("");
+let vogais= [ "a", "e", "i", "o", "u"]
+let exibir
+for (let i = 0; i < separaçao; i++){
+    if (frase == vogais){
+        frase = exibir
+    }
+}
+console.log(exibir);
